6f806e9c0095c28c362d8dfc2cd930cbd74332cf
bckben <benkeeney962@gmail.com>
2025-04-08 12:11:01 -0600

    trigger redeploy

--- backend/CineNiche/CineNiche/obj/Release/net8.0/CineNiche.AssemblyInfo.cs
+++ backend/CineNiche/CineNiche/obj/Release/net8.0/CineNiche.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b6e54ccb3ab69bffa9ab66b08b2672b9909979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1972b64faedb2c3b343ef975592ede9b891cc0cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
