9c50a7b26072ae15e098bf764a2cec707031aff4
AutPort <ajs289@byu.edu>
2025-04-10 20:54:07 -0600

    goodbye csp

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
-    },
-  },
 });
